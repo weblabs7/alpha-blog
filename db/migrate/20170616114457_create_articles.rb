e87b184b2fb5c602a67fd85367b3316675ed43f4
weblabs7 <weblabs@live.com>
2017-06-16 20:23:48 +0000

    added model validations and schema files for rb.

--- db/migrate/20170616114457_create_articles.rb
+++ db/migrate/20170616114457_create_articles.rb
@@ -4,7 +4,7 @@ class CreateArticles < ActiveRecord::Migration
       t.string :title
       t.text :description
 
-      t.timestamps null: false
+      t.timestamps 
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170615173856) do
+ActiveRecord::Schema.define(version: 20170616114457) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
-    t.text     "desctiption"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
